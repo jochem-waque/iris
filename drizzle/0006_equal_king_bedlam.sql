5680f77240a4ef9c43b4fffd24dce8b8fbafc651
Jochem Waqué <16637577+jochem-waque@users.noreply.github.com>
2025-11-05 23:54:26 +0100

    Fix migration

--- drizzle/0006_equal_king_bedlam.sql
+++ drizzle/0006_equal_king_bedlam.sql
@@ -1,6 +1,8 @@
+DELETE FROM `join_cooldown`; --> statement-breakpoint
 DROP INDEX `join_cooldown_channel_id_user_id_unique`;--> statement-breakpoint
 CREATE UNIQUE INDEX `join_cooldown_guild_id_user_id_unique` ON `join_cooldown` (`guild_id`,`user_id`);--> statement-breakpoint
 ALTER TABLE `join_cooldown` DROP COLUMN `channel_id`;--> statement-breakpoint
+DELETE FROM `stream_cooldown`; --> statement-breakpoint
 DROP INDEX `stream_cooldown_channel_id_user_id_unique`;--> statement-breakpoint
 CREATE UNIQUE INDEX `stream_cooldown_guild_id_user_id_unique` ON `stream_cooldown` (`guild_id`,`user_id`);--> statement-breakpoint
 ALTER TABLE `stream_cooldown` DROP COLUMN `channel_id`;
